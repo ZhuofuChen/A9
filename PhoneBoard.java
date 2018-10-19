7ac13d1a5a62a62fd8fcd1b65be7ead76c340143
zhuofuchen <chenzhuofusean@gmail.com>
2018-10-19 01:08:22 -0700

    PhoneBoard

--- PhoneBoard.java
+++ PhoneBoard.java
@@ -1,3 +1,39 @@
 public class PhoneBoard {
 
+    public static void main(String[] args) {
+        System.out.println(neighborNumber(25));
+    }
+
+    public static int neighborNumber(int digits) {
+        int[][] b = new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { -1, 0, -1 } };
+        int m = b.length;
+        int n = b[0].length;
+        int res = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                res += dfs(b, i, j, digits - 1);
+            }
+        }
+        return res;
+    }
+
+    static int[][] dirs = new int[][] { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+
+    public static int dfs(int[][] b, int r, int c, int digits) {
+        int m = b.length;
+        int n = b[0].length;
+        if (r < 0 || r >= m || c < 0 || c >= n || b[r][c] == -1) {
+            return 0;
+        }
+        if (digits == 0) {
+            return 1;
+        }
+        int res = 0;
+        for (int[] dir : dirs) {
+            int nr = r + dir[0];
+            int nc = c + dir[1];
+            res += dfs(b, nr, nc, digits - 1);
+        }
+        return res;
+    }
 }
---
+++ SameTree.java
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/same-tree/description/
+//第二轮：给定两颗树，判断它们是否有相同的shape
+//follow up：如果允许树的节点拥有任意数目的父节点，如何判断？  => 一样的做法，这次用Map来记录traverse过的node，以防重复计算 O（2^n）=>O（n^2）
+public class SameTree {
+    /**
+     * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+     * left; TreeNode right; TreeNode(int x) { val = x; } }
+     */
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return dummy;
+    }
+}
